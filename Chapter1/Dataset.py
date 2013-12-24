6b11e43679665bf7cf5a05a08e27698f61add91f
patrick <patrickjvarin@gmail.com>
2013-12-24 14:26:52 -0500

    added a classifier class to Dataset.py, this will eventually include a wrapper for automatically testing large datasets

--- Chapter1/Dataset.py
+++ Chapter1/Dataset.py
@@ -1,32 +1,43 @@
 import numpy as np
 import gzip, struct
 
+def parseMNIST(dataPath, labelsPath):
+	dataFile = gzip.GzipFile(dataPath)
+	labelFile = gzip.GzipFile(labelsPath)
+	
+	# push out the first 4 useless bytes
+	struct.unpack('>i',dataFile.read(4))[0]
+	numImgs, width, height = struct.unpack('>III',dataFile.read(12))
+
+	# push out the first 8 bytes of the labels file
+	struct.unpack('>II',labelFile.read(8))
+
+	# print useful output
+	print "Loading %s images, each %s by %s" % (numImgs,width,height)
+
+	# # allocate memory 
+	labels = np.zeros(numImgs)
+	data = np.zeros((numImgs, width*height))
+
+	# load data and labels
+	for i in xrange(numImgs):
+		labels[i] = struct.unpack('B',labelFile.read(1))[0]
+		d = dataFile.read(width*height)
+		data[i,:] = np.array(struct.unpack('B'*width*height,d))
+
+	print 'Done'
+	return data, labels
+
 class Dataset(object):
 	def loadMNIST(self, dataPath, labelsPath):
-		self.dataFile = gzip.GzipFile(dataPath)
-		self.labelFile = gzip.GzipFile(labelsPath)
-		
-		# push out the first 4 useless bytes
-		struct.unpack('>i',self.dataFile.read(4))[0]
-		numImgs, width, height = struct.unpack('>III',self.dataFile.read(12))
+		self.dataFile, self.labelFile = parseMNIST(dataPath,labelsPath)
 
-		# push out the first 8 bytes of the labels file
-		struct.unpack('>II',self.labelFile.read(8))
-
-		# print useful output
-		print "Loading %s images, each %s by %s" % (numImgs,width,height)
-
-		# # allocate memory 
-		self.labels = np.zeros(numImgs)
-		self.data = np.zeros((numImgs, width*height))
-
-		# load data and labels
-		for i in xrange(numImgs):
-			self.labels[i] = struct.unpack('B',self.labelFile.read(1))[0]
-			d = self.dataFile.read(width*height)
-			self.data[i,:] = np.array(struct.unpack('B'*width*height,d))
-
-		print 'Done'
+class Classifier(Dataset):
+	def loadMNIST(self, dataPath, labelsPath, test=False):
+		if test:
+			self.testData, self.testLabels = parseMNIST(dataPath,labelsPath)
+		else:
+			self.data, self.labels = parseMNIST(dataPath,labelsPath)
 
 if __name__ == '__main__':
 	# Test the MNISTDataset class
